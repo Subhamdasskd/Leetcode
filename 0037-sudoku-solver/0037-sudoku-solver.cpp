bbd69c2d13f0f1935fc3646ecc1c913f1d175f1d
Subham Das <subhamdasskd7808@gmail.com>
2025-08-31 14:47:30 +0530

    Time: 359 ms (53.82%), Space: 8.3 MB (99.98%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,36 +1,67 @@
+const size_t BUFFER_SIZE = 0x6fafffff;
+alignas(std::max_align_t) char buffer[BUFFER_SIZE];
+size_t buffer_pos = 0;
+
+void* operator new(size_t size) {
+    constexpr std::size_t alignment = alignof(std::max_align_t);
+    size_t padding = (alignment - (buffer_pos % alignment)) % alignment;
+    size_t total_size = size + padding;
+    char* aligned_ptr = &buffer[buffer_pos + padding];
+    buffer_pos += total_size;
+    return aligned_ptr;
+}
+
+void operator delete(void* ptr, unsigned long) {}
+void operator delete(void* ptr) {}
+void operator delete[](void* ptr) {}
 class Solution {
- public:
-  void solveSudoku(vector<vector<char>>& board) {
-    solve(board, 0);
-  }
-
- private:
-  bool solve(vector<vector<char>>& board, int s) {
-    if (s == 81)
-      return true;
-
-    const int i = s / 9;
-    const int j = s % 9;
-
-    if (board[i][j] != '.')
-      return solve(board, s + 1);
-
-    for (char c = '1'; c <= '9'; ++c)
-      if (isValid(board, i, j, c)) {
-        board[i][j] = c;
-        if (solve(board, s + 1))
-          return true;
-        board[i][j] = '.';
-      }
-
-    return false;
-  }
-
-  bool isValid(vector<vector<char>>& board, int row, int col, char c) {
-    for (int i = 0; i < 9; ++i)
-      if (board[i][col] == c || board[row][i] == c ||
-          board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
-        return false;
-    return true;
-  }
+public:
+    bool pos(vector<vector<char>>& board,int r,int co,char c)
+    {
+        for(int i=0;i<9;i++)
+        {
+            if(board[i][co]==c||board[r][i]==c)
+            {
+                return false;
+            }
+        }
+        int sr=r-(r%3);
+        int sc=co-(co%3);
+        for(int i=sr;i<sr+3;i++)
+        {
+            for(int j=sc;j<sc+3;j++)
+            {
+                if(board[i][j]==c)
+                return false;
+            }
+        }
+        return true;
+    }
+    bool solve(vector<vector<char>>& board){
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                if(board[i][j]=='.')
+                {
+                    for(char c='1';c<='9';c++)
+                    {
+                        if(pos(board,i,j,c))
+                        {
+                            board[i][j]=c;
+                            if(solve(board))return true;
+                            board[i][j]='.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    void solveSudoku(vector<vector<char>>& board) 
+    {
+        solve(board);
+        return;
+    }
 };
